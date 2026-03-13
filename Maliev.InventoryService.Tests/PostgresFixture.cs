007f05770bc31159ef0efa184e83464f9471542a
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-03-13 15:40:51 +0700

    chore: align with MALIEV Constitution v2.0.0 and migrate tests to Testcontainers (fix formatting and build)

--- Maliev.InventoryService.Tests/PostgresFixture.cs
+++ Maliev.InventoryService.Tests/PostgresFixture.cs
@@ -6,7 +6,7 @@ namespace Maliev.InventoryService.Tests;
 public class PostgresFixture : IAsyncLifetime
 {
     private readonly PostgreSqlContainer _dbContainer = 
-                #pragma warning disable CS0618
+#pragma warning disable CS0618
         new PostgreSqlBuilder()
         .WithImage("postgres:15-alpine")
         .WithDatabase("maliev_inventory_test")
