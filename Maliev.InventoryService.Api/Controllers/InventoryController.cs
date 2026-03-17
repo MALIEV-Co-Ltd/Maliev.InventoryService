3f78bd5fed949bc7d89ed9fcb679bb38d48ffffc
Natthapol Vanasrivilai <natthapol.vanasrivilai@maliev.com>
2026-03-17 17:08:07 +0700

    chore: format code after NuGet package updates

--- Maliev.InventoryService.Api/Controllers/InventoryController.cs
+++ Maliev.InventoryService.Api/Controllers/InventoryController.cs
@@ -14,7 +14,7 @@ namespace Maliev.InventoryService.Api.Controllers;
 /// Controller for managing material inventory.
 /// </summary>
 [ApiController]
-[ApiVersion("1.0")]
+[ApiVersion("1")]
 [Route("inventory/v{version:apiVersion}/stock")]
 [RequirePermission(InventoryPermissions.StockRead)]
 public class InventoryController : ControllerBase
